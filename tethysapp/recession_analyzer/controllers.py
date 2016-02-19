70f5731a79f2dac0a825023d86fb0ec850f33553
student <daviddralle@gmail.com>
2016-02-19 16:20:04 -0700

    updated all variable names for gizmos objects

--- tethysapp/recession_analyzer/controllers.py
+++ tethysapp/recession_analyzer/controllers.py
@@ -13,66 +13,95 @@ def home(request):
     """
     Controller for the app home page.
     """
-    gages  = TextInput(name='gages', display_text='Gage', initial='11477000',attributes={'size':15})
+    gages_initial = '11477000'
+    start_initial = '2000-01-01'
+    stop_initial = '2015-01-01'
+    concave_initial= False
+    nonlinear_fitting_initial= False
+    rec_sense_initial= 1
+    min_length_initial= 4
+    antecedent_moisture_initial = 1
+    lag_start_initial = 0
 
-    start = DatePicker(name='start',
+    if request.POST:
+        gages_initial = request.POST['gages_input']
+        start_initial = request.POST['start_input']
+        stop_initial = request.POST['stop_input']
+
+        if 'concave_input' in request.POST:
+            concave_initial=True;
+        else:
+            concave_initial=False;
+
+        if 'nonlinear_fitting_input' in request.POST:
+            nonlinear_fitting_initial=True;
+        else:
+            nonlinear_fitting_initial=False;
+
+        rec_sense_initial=request.POST['rec_sense_input']
+        min_length_initial=request.POST['min_length_input']
+        antecedent_moisture_initial = request.POST['antecedent_moisture_input']
+        lag_start_initial = request.POST['lag_start_input']
+
+
+
+
+    gages_options  = TextInput(name='gages_input', display_text='Gage', initial=gages_initial,attributes={'size':15})
+
+    start_options = DatePicker(name='start_input',
                                             display_text='Start date',
                                             autoclose=True,
                                             format='yyyy-m-d',
                                             start_date='01/01/1910',
-                                            initial='2000-01-01')
-    stop = DatePicker(name='stop',
+                                            initial=start_initial)
+    stop_options = DatePicker(name='stop_input',
                                             display_text='Stop date',
                                             autoclose=True,
                                             format='yyyy-m-d',
                                             start_date='01/01/1910',
-                                            initial='2015-01-01')  
-                                            
-                                                                           
-    concave_down_toggle = ToggleSwitch(name='concave_down_toggle', size='mini', display_text='Concave recessions')
+                                            initial=stop_initial)
 
-    fitting = ToggleSwitch(name='fitting', display_text='Nonlinear fitting',size='mini')
+
+    concave_options = ToggleSwitch(name='concave_input', size='mini', initial=concave_initial, display_text='Concave recessions')
+
+    nonlinear_fitting_options = ToggleSwitch(name='nonlinear_fitting_input', display_text='Nonlinear fitting',size='mini',initial = nonlinear_fitting_initial)
     
-    min_length = RangeSlider(name='min_length', min=4, max=10, initial=4, step=1,attributes={"onchange":"showValue(this.value,'min_length');"})
+    min_length_options = RangeSlider(name='min_length_input', min=4, max=10, initial=min_length_initial, step=1,attributes={"onchange":"showValue(this.value,'min_length_initial');"})
     
-    rec_sens = RangeSlider(name='rec_sens', min=0, max=1, initial=1, step=0.01,
-                           attributes={"onchange":"showValue(this.value,'rec_sense');"})
+    rec_sense_options = RangeSlider(name='rec_sense_input', min=0, max=1, initial=rec_sense_initial, step=0.01,
+                           attributes={"onchange":"showValue(this.value,'rec_sense_initial');"})
 
-    antecedent_moisture = RangeSlider(name='antecedent_moisture', min=0, max=1, initial=1, step=0.01,attributes={"onchange":"showValue(this.value,'antecedent_moisture');"})
+    antecedent_moisture_options = RangeSlider(name='antecedent_moisture_input', min=0, max=1, initial=antecedent_moisture_initial, step=0.01,attributes={"onchange":"showValue(this.value,'antecedent_moisture_initial');"})
 
-    lag_start = RangeSlider(name='lag_start', min=0, max=3, initial=0, step=1,attributes={"onchange":"showValue(this.value,'lag_start');"})
+    lag_start_options = RangeSlider(name='lag_start_input', min=0, max=3, initial=lag_start_initial, step=1,attributes={"onchange":"showValue(this.value,'lag_start_initial');"})
 
     submitted=''
 
     if request.POST:
         app_workspace = RecessionAnalyzer.get_user_workspace(request.user)
         new_file_path = os.path.join(app_workspace.path,'current_plot.txt')
-        concave = 'nonconcave'
-        fit='linfit'
-
-        if request.POST.get('concave_down_toggle'):
-            concave = 'concave'
-        if request.POST.get('fitting'):
-            fit='nonlinfit'
-
 
         with open(new_file_path,'w') as a_file:
-            poststring = request.POST['gages']+'\n' + request.POST['start']+','+request.POST['stop']+','+request.POST['rec_sens']+','+request.POST['min_length']+','+request.POST['antecedent_moisture']+','+request.POST['lag_start']+','+fit+','+concave
+            poststring = request.POST['gages_input']+'\n' + request.POST['start_input']+','+request.POST['stop_input']+','+request.POST['rec_sense_input']+','+request.POST['min_length_input']+','+request.POST['antecedent_moisture_input']+','+request.POST['lag_start_input']+','+str(nonlinear_fitting_initial)+','+str(concave_initial)
             a_file.write(poststring)
 
         submitted='submitted'
 
 
-    context = {'start': start, 
-                'stop':stop, 
-                'gages': gages,
-                'concave_down_toggle': concave_down_toggle,
-                'fitting':fitting,
-                'min_length':min_length,
+    context = {'start_options': start_options,
+               'rec_sense_initial':rec_sense_initial,
+               'antecedent_moisture_initial':antecedent_moisture_initial,
+               'lag_start_initial':lag_start_initial,
+               'min_length_initial':min_length_initial,
+                'stop_options':stop_options,
+                'gages_options' : gages_options,
+                'concave_options': concave_options,
+                'nonlinear_fitting_options':nonlinear_fitting_options,
+                'min_length_options':min_length_options,
                 'submitted':submitted,
-                'antecedent_moisture':antecedent_moisture,
-                'lag_start':lag_start,
-                'rec_sens':rec_sens}
+                'antecedent_moisture_options':antecedent_moisture_options,
+                'lag_start_options':lag_start_options,
+                'rec_sense_options':rec_sense_options}
 
     return render(request, 'recession_analyzer/home.html', context)
 
